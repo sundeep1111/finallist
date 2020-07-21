16c322ed597ace250670697de9e53197d509b47d
sundeep <sundeep1111-code@gmail.com>
2020-07-21 14:31:11 +0530

    Updates

---
+++ precache-manifest.e54221035f188c84e75e438563a1d46b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f12e33a764bf8652935724e6e1974f6f",
+    "url": "/finallist/index.html"
+  },
+  {
+    "revision": "97512e884fbc4718fe14",
+    "url": "/finallist/static/css/main.b2e6abe0.chunk.css"
+  },
+  {
+    "revision": "3b41ebbfc5336e768854",
+    "url": "/finallist/static/js/2.9aae7875.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/finallist/static/js/2.9aae7875.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97512e884fbc4718fe14",
+    "url": "/finallist/static/js/main.445b2005.chunk.js"
+  },
+  {
+    "revision": "5d4f513165b700b1a485",
+    "url": "/finallist/static/js/runtime-main.44843ea2.js"
+  }
+]);
